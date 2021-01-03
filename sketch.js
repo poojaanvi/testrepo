0538b7d27a815d460a7fc8c9db309476fb8b65ea
poojaanvi <poojaanvi655@gmail.com>
2021-01-03 17:36:34 +0530

    deleted platform2

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-var platform,platform2;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -19,7 +19,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
     platform = new Ground(150,200,400,20);
-    platform2 = new Ground(150,400,400,20);
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -49,7 +49,7 @@ function draw(){
     box2.display();
     ground.display();
     platform.display();
-    platform2.display();
+  
     pig1.display();
     log1.display();
 
